d45ba50522435489665222a5b396beef164e20ab
Marcin Oczeretko <marcin.oczeretko@gmail.com>
2015-10-26 19:27:45 +0100

    Add Mobile Service project to support old android SDK

---
+++ DsbForsinket.MobileServiceLegacy/App_Start/WebApiConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace DsbForsinket.MobileServiceLegacy
+{
+    public static class WebApiConfig
+    {
+        public static void Register()
+        {
+            ConfigOptions options = new ConfigOptions();
+            HttpConfiguration config = ServiceConfig.Initialize(new ConfigBuilder(options));
+        }
+    }
+}
+
