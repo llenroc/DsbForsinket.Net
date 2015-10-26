0a941680a119acd48ddf78ef97bde39c84c1efa6
Marcin Oczeretko <marcin.oczeretko@gmail.com>
2015-10-26 19:14:11 +0100

    Departures webjob draft and publish

--- DsbForsinket.DeparturesWebJob/Program.cs
+++ DsbForsinket.DeparturesWebJob/Program.cs
@@ -1,21 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Azure.WebJobs;
+﻿using Microsoft.Azure.WebJobs;
 
 namespace DsbForsinket.DeparturesWebJob
 {
-    // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
-    class Program
+    public class Program
     {
-        // Please set the following connection strings in app.config for this WebJob to run:
-        // AzureWebJobsDashboard and AzureWebJobsStorage
-        static void Main()
+        public static void Main()
         {
             var host = new JobHost();
-            // The following code ensures that the WebJob will be running continuously
             host.RunAndBlock();
         }
     }
