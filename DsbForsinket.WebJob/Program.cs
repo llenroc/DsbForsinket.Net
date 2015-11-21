799db7e28e243cdfc8939263d860fadff4647599
Marcin Oczeretko <marcin.oczeretko@gmail.com>
2015-11-21 15:13:50 +0100

    Change test notification job to send correct notification data

--- DsbForsinket.WebJob/Program.cs
+++ DsbForsinket.WebJob/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using DsbForsinket.Common;
 
 namespace DsbForsinket.WebJob
@@ -7,8 +8,22 @@ namespace DsbForsinket.WebJob
     {
         public static void Main()
         {
-            var message = $"TestNotificationJob - {DateTime.UtcNow.ToShortTimeString()}";
-            var notificationOutcome = new PushNotificationSender(Console.WriteLine).SendAsync(message).Result;
+            var tag = "8600856-10:45";
+
+            Dictionary<string, string> messageData = new Dictionary<string, string>
+            {
+                ["delayedCount"] = "1234",
+                ["departureName0"] = "Test Train 11:23",
+                ["departureDelay0"] = "0",
+                ["departureName1"] = "Marcin is testing 11:23",
+                ["departureDelay1"] = "5",
+                ["departureName2"] = "Train 3 11:23",
+                ["departureDelay2"] = "10",
+                ["departureName3"] = "Train 4 11:23",
+                ["departureDelay3"] = "61"
+            };
+
+            var notificationOutcome = new PushNotificationSender(Console.WriteLine).SendAsync(messageData, tag).Result;
             Console.WriteLine($"State: {notificationOutcome.State}");
             Console.WriteLine($"Success: {notificationOutcome.Success}");
             Console.WriteLine($"Failure: {notificationOutcome.Failure}");
